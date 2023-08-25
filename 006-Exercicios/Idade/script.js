8ef03c248a9657a0b79ae3556cb603bea47c6571
cahrro <batmana410@gmail.com>
2023-08-24 23:25:10 -0300

    corrigido

--- 006-Exercicios/Idade/script.js
+++ 006-Exercicios/Idade/script.js
@@ -1,42 +1,46 @@
 function verificarIdade(){
     data = new Date()
     ano = data.getFullYear()
-    anoNasc = document.getElementById('txtano')
-    resultado = document.getElementById('resultado')
-    if(anoNasc.value.length == 0 || anoNasc.value > ano ) {
-        window.alert('Verifique os dados e tente novamente')
+    anoNascimento = document.getElementById('txtano').value
+    result = document.getElementById('resultado')
+
+    if (anoNascimento.length == 0 || anoNascimento>ano){
+        window.alert('Revise os dados e tente novamente')
     } else{
-        sexo = document.getElementsByName('radsex')
-        idade = ano - anoNasc.value
+        RadioSex = document.getElementsByName('radsex') 
+        idade = ano - anoNascimento
         genero = ''
-        img = document.createElement('img')
-        img.setAttribute('id','foto')
-        if (sexo[0].checked){
+        imagem = document.createElement('img')
+        imagem.setAttribute('id','foto')
+
+
+        if (RadioSex[0].checked){
             genero = 'feminino'
             if (idade >= 0 && idade < 10){
-                img.setAttribute('src', img/CriancaMenina.jpg )
+                imagem.setAttribute('src', 'img/CriancaMenina.jpg')
             } else if (idade < 21){
-                img.setAttribute('src', img/JovemMulher.jpg )
+                imagem.setAttribute('src', 'img/JovemMulher.jpg')
             } else if (idade < 50){
-                img.setAttribute('src', img/AdultoMulher.jpg )
-            }  else{
-                img.setAttribute('src', img/IdosoMulher.jpg )
+                imagem.setAttribute('src', 'img/AdultoMulher.jpg')
+            } else{
+                imagem.setAttribute('src', 'img/IdosoMulher.jpg')
             }
-        } else if (sexo[1].checked){
+
+
+        }else if (RadioSex[1].checked){
             genero = 'masculino'
             if (idade >= 0 && idade < 10){
-                img.setAttribute('src', img/CriancaMenina.jpg )
+                imagem.setAttribute('src', 'img/CriancaMenino.jpg')
             } else if (idade < 21){
-                img.setAttribute('src', img/JovemMulher.jpg )
+                imagem.setAttribute('src', 'img/JovemHomem.jpg')
             } else if (idade < 50){
-                img.setAttribute('src', img/AdultoMulher.jpg )
-            }  else{
-                img.setAttribute('src', img/IdosoMulher.jpg )
+                imagem.setAttribute('src', 'img/AdultoHomem.jpg')
+            } else{
+                imagem.setAttribute('src', 'img/IdosoHomem.jpg')
+            }
         }
-        
-        }
-        
-        resultado.innerHTML = `Você é do gênero ${genero} e tem ${idade} anos`
-        resultado.appendChild('img')
+        result.innerHTML = `Pessoa do gênero ${genero} com ${idade} anos`
+        result.appendChild(imagem)
     }
+
 }
