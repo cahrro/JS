760fe0f7bd718bcf3f7a3b21dcc43749acabf564
cahrro <batmana410@gmail.com>
2023-09-22 20:42:47 -0300

    metodos variaveis

---
+++ 009-Array/sliceSpice.js
@@ -0,0 +1,8 @@
+let coisas = [true, ['array1'],8998,22,false,['array9'],'gato']
+
+coisas.splice(0,2)
+
+mostrar = coisas.slice(0,6)
+
+
+console.log(`${mostrar}`)
---
+++ 009-Array/variavelmetodos.js
@@ -0,0 +1,9 @@
+let coisas = [true, ['array1'],8998,22,false,['array9'],'gato']
+
+console.log(coisas[3])
+
+coisas.forEach(
+    function Infoarray (index,item){
+        console.log(item,index)
+    }
+)
